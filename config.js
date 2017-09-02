59a76bc19aef39d71e453b7072280777e4fb2982
pauli <wuliupo@163.com>
2017-09-02 14:46:12 +0800

    init

---
+++ config.js
@@ -0,0 +1,24 @@
+var CONFIG = {
+  // index page
+  index: "README.md",
+
+  // sidebar file
+  sidebar_file: "https://api.github.com/repos/wuliupo/law/git/trees/master?recursive=1&callback=sq",
+
+  // where the docs are actually stored on github - so you can edit
+  base_url: "https://github.com/wuliupo/law/edit/gh-pages",
+  git_url: "https://github.com/wuliupo/law",
+};
+
+// **************************
+// DON'T EDIT FOLLOWING CODES
+// **************************
+
+addConfig(ditto, CONFIG);
+
+function addConfig(obj, conf) {
+  Object.keys(conf).forEach(function (key) {
+    obj[key] = conf[key];
+  });
+}
+
